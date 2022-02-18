a4c9b122d2c19bcd1001207f06c9515a52bccbd8
Lyere <BPpearsall@gmail.com>
2022-02-18 09:29:45 -0500

    added character confirm prompts and started if statement to prove that the user has selected at least one character type

--- Develop/script.js
+++ Develop/script.js
@@ -2,11 +2,32 @@
 var generateBtn = document.querySelector("#generate");
 
 // Global Character Variables
-let upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-let lower = "abcdefghijklmnopqrstuvwxyz";
+let upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let lowerCase = "abcdefghijklmnopqrstuvwxyz";
 let numbers = "0123456789";
-let special = "!@,#$%&*{}[]/\\+=";
+let specialCharacter = "!@,#$%&*/+=";
 let chosenCharacters = "";
+
+function generatePassword() {
+  let finalPassword = "";
+  let passwordLength = prompt("Choose a length for your password between the values of: 8 and 128")
+  if (passwordLength < 8 || passwordLength > 128) {
+    alert("You must select a number between 8 and 128 in order for the generator to work.") 
+  } 
+
+  if(isNaN(passwordLength)){
+      alert("You must input a valid numerical value")
+  }
+  let hasUpperCase = confirm("Do you want to include upper case letters (A,B,C...) in your password?");
+  let hasLowerCase = confirm("Do you want to include lower case letters (a,b,c...) in your password?");
+  let hasNumbers = confirm("Do you want to include numbers (1,2,3...) in your passord?");
+  let hasSpecialCharacter = confirm("Do you want to include special characters (!,@,#...) into your?");
+
+  if (hasUpperCase && hasLowerCase && hasNumbers && hasSpecialCharacter === false) {
+    
+  }
+
+}
 
 // Write password to the #password input
 function writePassword() {
@@ -15,11 +36,6 @@ function writePassword() {
 
   passwordText.value = password;
 
-}
-
-
-function generatePassword() {
-  let result
 }
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
